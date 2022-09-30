5ad69f1416111416cb2a6aa31da02eda3453b6f6
Ali Zaheer <zaheerlko@yahoo.com>
2022-09-29 19:27:28 -0700

    Update Unique.java

--- Unique.java
+++ Unique.java
@@ -9,9 +9,11 @@ public class Unique {
         int arr[] = { 12, 10, 9, 45, 2, 10, 10, 45 };
         int res = 1;
         
+//Modify the code to run in O(n) meaning no nested loops. You can use extra space. Assume the numbers will be between 1-100
         for (int i = 1; i < arr.length; i++) 
         {
          int j = 0;
+        
             for (j = 0; j < i; j++)
                 if (arr[i] == arr[j])
                     break;
